ee287468e7cb71e747812ceaa5377318cdd87031
hunsri <richard.hunstock@gmx.de>
2023-03-13 11:47:21 +0100

    added predefined rotations

--- Assets/Scripts/OverlapDetector.cs
+++ Assets/Scripts/OverlapDetector.cs
@@ -9,6 +9,8 @@ public class OverlapDetector : MonoBehaviour
 
     private GameObject _overlappingObject;
 
+    private bool _hasFixatedObject;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Grabbable")
@@ -23,6 +25,8 @@ public class OverlapDetector : MonoBehaviour
         if (other.tag == "Grabbable")
         {
             ResetObject();
+            
+            ResetDetector();
         }
     }
 
@@ -45,6 +49,7 @@ public class OverlapDetector : MonoBehaviour
         if(_overlappingObject != null)
         {
             TransformationAnalyser.Analyse(_overlappingObject.transform, this.transform);
+            _hasFixatedObject = true;
         }
     }
 
@@ -55,6 +60,23 @@ public class OverlapDetector : MonoBehaviour
         _overlappingObject.GetComponent<CustomGrabbableObject>().IsGrabbable = true;
 
         _overlappingObject = null;
+    }
+
+    private void ResetDetector()
+    {
+        if(_hasFixatedObject)
+        {
+            //rotating the destination for the next placement
+            if(this.GetComponent<DestinationRotator>().Rotate())
+            {
+                _hasFixatedObject = false;
+            }
+            else
+            {   
+                //disabling the detector once all rotations have taken place
+                transform.parent.parent.gameObject.SetActive(false);
+            }
+        }
     }
 
 }
